1129304fc5ad13267a4ee2848816185404e7d37c
Nazareno <nazarenolrios@gmail.com>
2024-07-02 01:04:01 -0300

    dotenv

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,11 @@
-// db.js
+require("dotenv").config(); // Carga las variables de entorno desde .env
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "project_management",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = db;
